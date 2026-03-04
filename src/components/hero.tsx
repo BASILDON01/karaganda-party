7427ca6943536eacd465e0b76f5f2d868579ecac
Daniil2 <olehsu36@gmail.com>
2026-03-05 00:20:43 +0500

    Убран анимированный индикатор прокрутки в Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -100,13 +100,6 @@ export function Hero() {
           </div>
         </div>
       </div>
-
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 rounded-full border-2 border-white/30 flex items-start justify-center p-2">
-          <div className="w-1 h-2 bg-white/50 rounded-full animate-pulse" />
-        </div>
-      </div>
     </section>
   );
 }
